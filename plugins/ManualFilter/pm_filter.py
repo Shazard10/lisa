c42cdbc0f47568ddf0c0d3c995648da88e879846
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-04-14 22:44:20 +0530

    Update pm_filter.py

--- plugins/ManualFilter/pm_filter.py
+++ plugins/ManualFilter/pm_filter.py
@@ -27,12 +27,6 @@ logger.setLevel(logging.ERROR)
 BUTTONS = {}
 
 
-@Client.on_message(filters.group & filters.text & ~filters.edited & filters.incoming)
-async def give_filter(client, message):
-    k = await manual_filters(client, message)
-    if k == False:
-        await auto_filter(client, message)
-
 @Client.on_callback_query()
 async def cb_handler(client: Client, query: CallbackQuery):
     if query.data == "close_data":
