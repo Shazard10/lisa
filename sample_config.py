8f2b4461b6604c651b87853685f575ec472e5472
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-04-14 19:48:36 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -35,9 +35,7 @@ class Config(object):
 
     # Go to https://dashboard.heroku.com/account, scroll down and press Reveal API
     # To check dyno status
-    HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", "")
-
-
+   
     # OPTIONAL - To set alternate BOT COMMANDS
     ADD_FILTER_CMD = os.environ.get("ADD_FILTER_CMD", "add")
     DELETE_FILTER_CMD = os.environ.get("DELETE_FILTER_CMDD", "del")
