089a732d05803fe6ec78c97366755c689da7d869
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2022-04-15 11:07:44 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -110,8 +110,7 @@ async def start_command(client: Client, message: Message):
                 mention = message.from_user.mention,
                 id = message.from_user.id
             ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
+            reply_markup = reply_markup,           
             quote = True
         )
         return
@@ -147,8 +146,7 @@ async def not_joined(client: Client, message: Message):
                 id = message.from_user.id
             ),
         reply_markup = InlineKeyboardMarkup(buttons),
-        quote = True,
-        disable_web_page_preview = True
+        quote = True
     )
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
